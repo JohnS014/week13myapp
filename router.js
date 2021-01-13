0b24e6ba65b3eb7e0f984e7968023135cd2214d1
JohnS014 <66779636+JohnS014@users.noreply.github.com>
2021-01-13 19:59:57 +0000

    update endpoints

--- router.js
+++ router.js
@@ -5,8 +5,8 @@ const todos = require ('./todosController')
 /* GET home page. */
 router.get('/todo', todos.index)
 router.post('/todo/create', todos.create)
-router.get('/todo/id', todos.show)
+router.get('/todo/:id', todos.show)
 router.delete('/todo/:id', todos.delete)
-router.put('/todo/id', todos.update)
+router.put('/todo/:id', todos.update)
 
 module.exports = router;
