efb682ed759d05a8339d9c2ba89a650241131f12
JohnS014 <66779636+JohnS014@users.noreply.github.com>
2021-01-13 19:15:17 +0000

    first commit

---
+++ todosController.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors')
+
+let todolist = []
+let idno = 0
+
+exports.index = function (req, res) {
+    res.send({key: "item"})
+}
+
+exports.create = function (req, res) {
+    if(!req.body.name) {
+        return(next(createError(400, "name is required")))
+    }
+    todolist.push(req.body)
+    res.send({result: true})
+    idno++
+}
+
+exports.show = function (req, res, next) {
+     const todoitem = todolist.find((todo) => todo.id == req.params.id)
+     if(!todoitem) {
+        return(next(createError(404, "no todo with that id")))
+     }
+     res.send(todoitem)
+    }
+}
+
+exports.delete = function (req, res, next) {
+    const todoitem = todolist.find((todo) => todo.id == req.params.id)
+    if(!todoitem) {
+        return(next(createError(404, "no todo with that id")))
+    }
+    todolist = todolist.filter((todo) => todo.id != req.params.id)
+    res.send({result: true})
+}
+
+exports.update = function (req, res, next) {
+    const todoitem = todolist.find((todo) => todo.id == req.params.id)
+    if (!req.body.name) {
+        return (next(createError(404, "no todo with that id")))
+    }
+    todolist = todolist.map ((todo) => {
+     if (todo.id == req.params.id) {
+         todo.name = req.body.name
+     }   
+     return todo
+    })
+    res.send({ result: true })
+}
